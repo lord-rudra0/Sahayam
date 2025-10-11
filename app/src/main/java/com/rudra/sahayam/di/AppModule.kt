639e6a913acd30320579c97ba0c53cf945c51fb7
Rudra Pratap Singh <rudrapratapsingh2026@cs.ajce.in>
2025-10-11 10:41:50 +0530

    stage one all file added

--- app/src/main/java/com/rudra/sahayam/di/AppModule.kt
+++ app/src/main/java/com/rudra/sahayam/di/AppModule.kt
@@ -1,2 +1,37 @@
 package com.rudra.sahayam.di
 
+import android.content.Context
+import androidx.room.Room
+import com.sahayam.app.data.api.ApiService
+import com.sahayam.app.data.db.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl("https://api.sahayam.in/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(OkHttpClient.Builder().build())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService =
+        retrofit.create(ApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideDatabase(context: Context): AppDatabase =
+        Room.databaseBuilder(context, AppDatabase::class.java, "sahayam.db").build()
+}
